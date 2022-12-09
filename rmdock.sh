c9537c6cfc6414097b4641d1753e42b550329418
ajioxxa <dolgolevets65@gmail.com>
2022-12-09 01:30:17 -0800

    fix 2

--- rmdock.sh
+++ rmdock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-contname =$(sudo docker ps -a -q)
+$contname =$(sudo docker ps -a -q)
 if [[ "$contname" != "" ]]; then
   sudo docker stop $(sudo docker ps -a -q)
   sudo rm $(sudo docker ps -a -q)
