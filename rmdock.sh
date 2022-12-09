521b6ed5599fc0c45329f442ecebd5e90c178c11
ajioxxa <dolgolevets65@gmail.com>
2022-12-09 01:31:44 -0800

    fix 2

--- rmdock.sh
+++ rmdock.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-$contname=$(sudo docker ps -a -q)
+$contname = $(sudo docker ps -a -q)
 if [[ "$contname" != "" ]]; then
   sudo docker stop $(sudo docker ps -a -q)
   sudo rm $(sudo docker ps -a -q)
 else
   echo "Nothing to remove in dpocker"
-fo
+fi
