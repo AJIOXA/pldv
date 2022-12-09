1cdea1ebda7030bdd672bf1767c0c82bfa60ddc3
ajioxxa <dolgolevets65@gmail.com>
2022-12-09 01:29:14 -0800

    fixed bash script

--- rmdock.sh
+++ rmdock.sh
@@ -3,7 +3,6 @@ contname =$(sudo docker ps -a -q)
 if [[ "$contname" != "" ]]; then
   sudo docker stop $(sudo docker ps -a -q)
   sudo rm $(sudo docker ps -a -q)
-  sudo image prune
 else
   echo "Nothing to remove in dpocker"
 fo
